02e7baad8b7f83360230547f47ffdd3c5d03c2de
erik9631 <erik9631@gmail.com>
2024-07-07 13:33:20 +0200

    build: Fixed build issues

--- src/avec.rs
+++ src/avec.rs
@@ -13,17 +13,45 @@ const STOP: usize = 0;
 const GROW: usize = 1;
 
 pub struct AVec<'a, T, const CHUNK_COUNT: usize = 4> {
-    chunks_raw: *mut FVec<T>,
+    chunks_raw: SendPtr<FVec<T>>,
     chunks: &'a mut [FVec<T>],
     current_chunk: usize,
     largest_chunk: usize, // Should be owned by the grow thread. Make it a local thread variable
     tx: Sender<(usize, usize)>,
     grow_thread: JoinHandle<()>
 }
-impl<'a, T, const CHUNK_COUNT: usize> AVec<'a, T, CHUNK_COUNT> {
+struct SendPtr<T> {
+    ptr: *mut T
+}
+
+impl <T> SendPtr<T> {
+    fn as_slice(&self, size: usize) -> &[T] {
+        unsafe {
+            std::slice::from_raw_parts(self.ptr, size)
+        }
+    }
+
+    fn as_slice_mut(&self, size: usize) -> &mut [T] {
+        unsafe {
+            std::slice::from_raw_parts_mut(self.ptr, size)
+        }
+    }
+}
+
+unsafe impl <T> Send for SendPtr<T> {}
+
+impl<T> Clone for SendPtr<T> {
+    fn clone(&self) -> Self {
+        return SendPtr{ptr: self.ptr};
+    }
+}
+
+impl<T> Copy for SendPtr<T> {}
+
+impl<T, const CHUNK_COUNT: usize> AVec<'static, T, CHUNK_COUNT> {
     const CHUNKS: usize = CHUNK_COUNT;
     const MASK : usize = CHUNK_COUNT - 1;
-    pub fn new(size: usize) -> AVec<'a, T, CHUNK_COUNT> {
+    pub fn new(size: usize) -> AVec<'static, T, CHUNK_COUNT> {
 
         let layout = Layout::array::<FVec<T>>(CHUNK_COUNT).expect("Failed to create memory layout");
         let chunks_raw = unsafe {
@@ -47,11 +75,12 @@ impl<'a, T, const CHUNK_COUNT: usize> AVec<'a, T, CHUNK_COUNT> {
             iter = unsafe {iter.add(1)};
         }
         let chunks = unsafe { from_raw_parts_mut(chunks_raw, CHUNK_COUNT)};
+        let send_ptr = SendPtr{ptr: chunks_raw};
 
-        let (tx, grow_thread) = Self::start_grow_thread(chunks_raw);
+        let (tx, grow_thread) = Self::start_grow_thread(send_ptr.clone());
 
         AVec {
-            chunks_raw,
+            chunks_raw: send_ptr,
             chunks,
             current_chunk: 0,
             largest_chunk: 3,
@@ -86,13 +115,12 @@ impl<'a, T, const CHUNK_COUNT: usize> AVec<'a, T, CHUNK_COUNT> {
         }
     }
 
-    fn start_grow_thread(chunks_raw: *mut FVec<T>) -> (Sender<(usize, usize)>, JoinHandle<()>){
+    fn start_grow_thread(chunks_send: SendPtr<FVec<T>>) -> (Sender<(usize, usize)>, JoinHandle<()>){
         let (tx, rx) = mpsc::channel::<(usize, usize)>();
-        let chunks = chunks_raw;
         let handle = thread::spawn(move || {
             let mut replace_index = 0;
             let mut largest_index = CHUNK_COUNT - 1;
-            let chunks = unsafe {from_raw_parts_mut(chunks, CHUNK_COUNT)}; // They will never overlap so it is fine
+            let chunks = chunks_send.as_slice_mut(CHUNK_COUNT); // They will never overlap so it is fine
             while let message = rx.recv().expect("Grow thread hung up!") {
                 if message.0 == STOP {
                     break;
